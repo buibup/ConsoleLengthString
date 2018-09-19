86b9d6ed8630ee7d587f862c71bce7f4d2ae2ce3
buibup <mongkolm@hatari.net>
2018-09-19 10:36:40 +0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LengthString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Here:
+            Console.Write("Input : ");
+            var str = Console.ReadLine();
+            Console.WriteLine($"Length is {str.Length}");
+            goto Here;
+        }
+    }
+}
